42e29a5193ec3dd3634cc1364e333137b4366466
Noahjundurian <106822871+Noahjundurian@users.noreply.github.com>
2022-08-30 12:36:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("meu primeiro projeto no console")
+
+var firstName = "ranbir";
+
+var lastName = "kapoor";
+
+var nome = firstName.concat(lastName); console.log(nome);
+
+var a =10;
+
+var b =5;
+
+var add=a+b;
+
+console.log(add)
+
+
+
